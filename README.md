b5a0320b9ce9946d6357a9fd0cb773c0dd3082ac
PET <pet@titan7.petnet.rh.dk>
2025-04-13 13:21:10 +0200

    orthanc v1

---
+++ README.md
@@ -0,0 +1,2 @@
+1. Start the orthanc container
+2. (if first time) sh orthanc/upload_data.sh
---
+++ orthanc/upload_data.sh
@@ -0,0 +1,2 @@
+## Change the directory to where your data is
+storescu -xs -aec ORTHANC localhost 4242 --scan-directories /depict/users/hinge/shared/mcp-data/*/*/*/*
